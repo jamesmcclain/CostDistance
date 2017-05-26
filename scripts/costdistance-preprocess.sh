0fbf49eeeef8e03e2eff64cde4c0b887eea92435
James McClain <jmcclain@azavea.com>
2017-05-26 09:40:20 -0400

    Fix: Update Scripts

--- scripts/costdistance-preprocess.sh
+++ scripts/costdistance-preprocess.sh
@@ -6,23 +6,23 @@ ZOOM=${2:-9}
 $SPARK_HOME/bin/spark-submit \
    --master 'local[*]' \
    --driver-memory 16G \
-   cdistance/target/scala-2.11/cdistance-assembly-0.jar \
+   cdistance/target/scala-2.11/cdistance-assembly-0.22.7.jar \
    'file:///tmp/hdfs-catalog' slope $ELEVATION slope $ZOOM
 
 $SPARK_HOME/bin/spark-submit \
    --master 'local[*]' \
    --driver-memory 16G \
-   cdistance/target/scala-2.11/cdistance-assembly-0.jar \
+   cdistance/target/scala-2.11/cdistance-assembly-0.22.7.jar \
    'file:///tmp/hdfs-catalog' costdistance slope cost $ZOOM $(pwd)/data/cities-3857/cities-3857.shp 20000
 
 $SPARK_HOME/bin/spark-submit \
    --master 'local[*]' \
    --driver-memory 16G \
-   cdistance/target/scala-2.11/cdistance-assembly-0.jar \
+   cdistance/target/scala-2.11/cdistance-assembly-0.22.7.jar \
    'file:///tmp/hdfs-catalog' histogram cost $ZOOM cost-pyramid 0
 
 $SPARK_HOME/bin/spark-submit \
    --master 'local[*]' \
    --driver-memory 16G \
-   cdistance/target/scala-2.11/cdistance-assembly-0.jar \
+   cdistance/target/scala-2.11/cdistance-assembly-0.22.7.jar \
    'file:///tmp/hdfs-catalog' pyramid cost $ZOOM cost-pyramid 256
