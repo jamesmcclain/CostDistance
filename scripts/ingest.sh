521724794d96ab10525f1b6f67b17d187d4bd1fa
James McClain <jmcclain@azavea.com>
2017-05-26 09:31:32 -0400

    Update ETL Jar

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -6,7 +6,7 @@ $SPARK_HOME/bin/spark-submit \
    --class geotrellis.spark.etl.SinglebandIngest \
    --master 'local[*]' \
    --driver-memory 16G \
-   geotrellis-spark-etl-assembly-1.0.0-SNAPSHOT.jar \
+   geotrellis-spark-etl-assembly-1.2.0-SNAPSHOT.jar \
    --input "file://$(pwd)/json/${NAME}.json" \
    --output "file://$(pwd)/json/output.json" \
    --backend-profiles "file://$(pwd)/json/backend-profiles.json"
